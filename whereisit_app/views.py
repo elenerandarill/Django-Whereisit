cb2346a7cb405c6f701a5346d3c2d437e424e89a
elener <paulina.wojno@gmail.com>
2020-06-26 20:16:25 +0200

    User registration works.

--- whereisit_app/views.py
+++ whereisit_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def home(request):
@@ -8,7 +11,18 @@ def home(request):
 
 def register(request):
     title = 'Register'
-    return render(request, 'whereisit_app/register.html', {'title': title})
+    if request.method == 'GET':
+        form = UserRegisterForm()
+        return render(request, 'whereisit_app/register.html', {'title': title, 'form': form})
+    else:
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created! You may now log in.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Something went wrong')
+            return render(request, 'whereisit_app/register.html', {'title': title, 'form': form})
 
 
 def login(request):
