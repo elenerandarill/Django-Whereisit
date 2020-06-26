a308855d8a0de47733df81fd70a074bcb6c29d9e
elener <paulina.wojno@gmail.com>
2020-06-26 19:19:17 +0200

    First templates.

--- whereisit/urls.py
+++ whereisit/urls.py
@@ -20,5 +20,7 @@ from whereisit_app import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
     path('admin/', admin.site.urls),
 ]
--- whereisit_app/views.py
+++ whereisit_app/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def home(request):
     title = "Home"
     return render(request, 'whereisit_app/home.html', {'title': title})
+
+
+def register(request):
+    title = 'Register'
+    return render(request, 'whereisit_app/register.html', {'title': title})
+
+
+def login(request):
+    title = 'Log In'
+    return render(request, 'whereisit_app/login.html', {'title': title})
