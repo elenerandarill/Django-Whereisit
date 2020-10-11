ce7b293088fbc01ea091184cd9ba14d0282a013a
elener <paulina.wojno@gmail.com>
2020-10-11 16:28:56 +0200

    Added Location objects. Can be created only by superuser in admin's page.

--- whereisit_app/admin.py
+++ whereisit_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Profile, Item, GroupOfUsers, User
+from .models import Profile, Item, GroupOfUsers, User, Location
 
 
 admin.site.register(Profile)
 admin.site.register(Item)
 admin.site.register(GroupOfUsers)
 admin.site.register(User)
+admin.site.register(Location)
