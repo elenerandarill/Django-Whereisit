e41e4d559324db66fe421807cdb4e187bac733e4
elener <paulina.wojno@gmail.com>
2020-10-13 21:03:56 +0200

    Return to basic django User model. Bug with saving images fixed. Small upgrade to profile form. Deleted 1 of signals.

--- whereisit_app/admin.py
+++ whereisit_app/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Profile, Item, GroupOfUsers, User, Location
+from .models import Profile, Item, GroupOfUsers, Location
 
 
 admin.site.register(Profile)
 admin.site.register(Item)
 admin.site.register(GroupOfUsers)
-admin.site.register(User, UserAdmin)
 admin.site.register(Location)
